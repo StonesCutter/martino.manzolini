d5659f02382512c350ffdeca7fa790830bfd3cbf
StonesCutter <martino.manzolini@gmail.com>
2022-12-22 00:22:25 +0100

    test Mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,7 +38,80 @@ class Header extends Component {
 
     if (isMobile) {
       return (
-        <div></div>
+      
+        <header id="home">
+        <ParticlesBg type="cobweb" color="#476b6b" bg={true} />
+
+        <nav id="nav-wrap">
+          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+            Show navigation
+          </a>
+          <a className="mobile-btn" href="#home" title="Hide navigation">
+            Hide navigation
+          </a>
+
+          <ul id="nav" className="nav">
+            <li className="current">
+              <a className="smoothscroll" href="#home">
+                Home
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#about">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#resume">
+                Resume
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#portfolio">
+                Works
+              </a>
+            </li>
+
+            <li>
+              <a className="smoothscroll" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="row banner">
+          <div className="banner-text">
+            <Fade bottom>
+              <h2 className="responsive-headline">{name}</h2>
+            </Fade>
+            <Fade bottom duration={1200}>
+              <h3>{description}</h3>
+            </Fade>
+            <hr />
+            <Fade bottom duration={2000}>
+              <ul className="social">
+                <a href="#portfolio" className="button btnMobile project-btn">
+                  <i className="fa fa-book"></i>Projects
+                </a>
+                <a href={github} className="button btnMobile github-btn">
+                  <i className="fa fa-github"></i>Github
+                </a>
+              </ul>
+            </Fade>
+          </div>
+        </div>
+
+        <p className="scrolldown">
+          <a className="smoothscroll" href="#about">
+            <i className="icon-down-circle"></i>
+          </a>
+        </p>
+      </header>
+   
       );
     } else {
 
