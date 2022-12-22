21103fe32702c50e87b10cec27530f22fa2cc0e8
StonesCutter <martino.manzolini@gmail.com>
2022-12-22 11:49:54 +0100

    bio updated

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,6 +8,7 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
+    const bio2 = this.props.data.bio2;
     const street = this.props.data.address.street;
     const city = this.props.data.address.city;
     const state = this.props.data.address.state;
@@ -29,32 +30,35 @@ class About extends Component {
             </div>
             <div className="nine columns main-col">
               <h2>About Me</h2>
-
               <p>{bio}</p>
-              <div className="row">
+              <p>{bio2}</p>
+                <div className="row">
                 <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-                    <span>{name}</span>
+                    {/*<span>{name}</span>
                     <br />
                     <span>
                       {street}
                       <br />
                       {city} {state}, {zip}
                     </span>
-                    <br />
+                    
+                    <br />*/}
                     <span>{phone}</span>
                     <br />
                     <span>{email}</span>
                   </p>
                 </div>
+                {/*
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button">
+                      <a href={resumeDownload} className="button">
                       <i className="fa fa-download"></i>Download Resume
                     </a>
                   </p>
                 </div>
+                */}
               </div>
             </div>
           </div>
