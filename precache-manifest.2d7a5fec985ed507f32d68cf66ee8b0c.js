3e389b8ea7ee822f9f60e117fc8cdd87955d4d83
StonesCutter <martino.manzolini@gmail.com>
2022-12-24 15:14:04 +0100

    Updates

---
+++ precache-manifest.2d7a5fec985ed507f32d68cf66ee8b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "edc4e6f9062c0fd5a189",
+    "url": "/martino.manzolini/static/css/main.db9122f5.chunk.css"
+  },
+  {
+    "revision": "edc4e6f9062c0fd5a189",
+    "url": "/martino.manzolini/static/js/main.edc4e6f9.chunk.js"
+  },
+  {
+    "revision": "2aad000bc8d799e68c4e",
+    "url": "/martino.manzolini/static/js/1.2aad000b.chunk.js"
+  },
+  {
+    "revision": "6caa863dbeea2001566f",
+    "url": "/martino.manzolini/static/js/2.6caa863d.chunk.js"
+  },
+  {
+    "revision": "03bd83a62f1ae4e8e768",
+    "url": "/martino.manzolini/static/js/runtime~main.03bd83a6.js"
+  },
+  {
+    "revision": "2101a2a32d99460257466b378c5f53d0",
+    "url": "/martino.manzolini/index.html"
+  }
+];
